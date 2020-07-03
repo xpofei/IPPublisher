d63f52945b486e62ac5761fba2a8bd3b8ebd298f
pofei <79534672@qq.com>
2020-07-03 04:37:41 +0100

    update ip

--- getip.sh
+++ getip.sh
@@ -1,3 +1,16 @@
-curl http://members.3322.org/dyndns/getip > ip.txt
+#!/bin/bash
+LAST_IP=$(curl http://members.3322.org/dyndns/getip)
+echo $LAST_IP > ip.txt
 git commit -a -m "update ip"
 git push
+while
+do
+	IP=$(curl http://members.3322.org/dyndns/getip)
+	if [ "$LAST_IP" -ne "$IP" ]; then
+		echo $IP > ip.txt
+		git commit -a -m "update ip"
+		git push
+		LAST_IP=$IP
+	fi
+	sleep 60
+done
